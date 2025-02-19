671c9b66b42477b3668f8481e8641c6b07df7cb8
NhatDuonq <thangdeptrai.nn@gmail.com>
2025-02-19 17:05:26 +0700

    first commit

---
+++ test_vite/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BaiTap1 from './components/BaiTap1.jsx'
+
+function App() {
+
+return(
+  <>
+  <BaiTap1 />
+  </>
+)
+
+ 
+
+}
+
+
+export default App
