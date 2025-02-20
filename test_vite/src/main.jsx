0b8d13d77f42a4bd254fab951c35df97e26900ec
NhatDuonq <thangdeptrai.nn@gmail.com>
2025-02-20 09:28:06 +0700

    Bo sung minh chung

--- test_vite/src/main.jsx
+++ test_vite/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import BTBoostrap from './components/BTBoostrap.jsx'
+import BaiTap1 from './components/BaiTap1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   
+    <BaiTap1></BaiTap1>
   </StrictMode>,
 )
